b5fcab6bc8c02dacaeed9e93bbb9170175923e2e
openacs-s6-s <>
2022-01-29 17:22:48 +0000

    ci: debug
    FossilOrigin-Name: 5225b3a585978a94ed42d81099f8ec1fbf0e184652a7e28b736760aaa7647391

--- src/hook/build.sh
+++ src/hook/build.sh
@@ -7,7 +7,7 @@ IMAGE="${IMAGE:-oupfiz5/openacs-s6:${VERSION}}"
 docker build \
        --no-cache \
        --build-arg BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%SZ")" \
-       --build-arg OACS_TAG="${VERSION}"\
+       --build-arg OACS_TAG="oacs-5-10"\
        -t "${IMAGE}" \
        -f ../Dockerfile \
        ../.
